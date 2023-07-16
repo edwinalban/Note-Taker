7b32d4b6752ef9ecbb5261a0a8f9e0c8da9da043
Yachtzee <edwinalban@gmail.com>
2023-07-16 15:19:44 -0700

    removed index.js from routes folder, updated html route order so wildcard is invoked only after other defined routes have been checked, updated index.html file to route to notes page on click

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use('/', api);
 
 app.use(express.static('public'));
 
-app.get('*', (req, res) => (
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-));
-
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('*', (req, res) => (
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+));
 
 app.listen(PORT, () => {
     console.log(`App listening at http://localhost:${PORT}`)

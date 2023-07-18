c2fadb92cb5b2d00f8835dc362ab0b53b0ec6b6b
Yachtzee <edwinalban@gmail.com>
2023-07-17 22:04:38 -0700

    reordered public to come before route definitions

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use('/api', api);
-
 app.use(express.static('public'));
+app.use('/api', api);
 
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/notes.html'))
